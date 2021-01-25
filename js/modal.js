12d50629058867d2839c780ad46768e75f4548d2
ZinedineMess <zinedinemessahel@hotmail.com>
2021-01-25 17:13:51 +0100

    feat: ajout de la fonctionnalité fermer la modale et restructure des dossiers du projet

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+closeBtn[0].addEventListener("click", closeModal);
+console.log(closeBtn);
+console.log(closeBtn[3]);
+
 
 
